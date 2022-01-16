6ff074d19df2ea7be1cef1a806a1c2c198382052
Mikuto Haga <e215756@ie.u-ryukyu.ac.jp>
2022-01-16 14:37:45 +0900

    5th commit

--- src/InputBoard.java
+++ src/InputBoard.java
@@ -8,25 +8,10 @@ public class InputBoard {
     ArrayList<Integer> intBoard = new ArrayList<Integer>();
     //staticをつけることで後に作る判定メソットでも使用できるようにする。
     static List<String> strBoard = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i");
-    /*public void addstrBoard(){
-        strBoard.add("a");
-        strBoard.add("b");
-        strBoard.add("c");
-        strBoard.add("d");
-        strBoard.add("e");
-        strBoard.add("f");
-        strBoard.add("g");
-        strBoard.add("h");
-        strBoard.add("i");
-    }*/
-
-    
     
     //キーボード入力を受け付けるためのメソッド
     public void input(String Player){
-        
         int num;
-
         if(Player == "o"){
             System.out.println("0~8を入力してください"); 
             Scanner scan = new Scanner(System.in);     
@@ -35,22 +20,18 @@ public class InputBoard {
         else{
             num = (int)(Math.random()*9);
         }
-
         if(strBoard.get(num) == "x" || strBoard.get(num) == "o"){
             System.out.println("そこにはすでに値が入っています。もう一度入力してください");
             //座標に値が入っている場合にもう一度メソッドを呼び出す（擬似ループ）
             this.input(Player);
         }
-        
-            //指定した座標に代入
+        //指定した座標に代入
         else {
             strBoard.set(num, Player);
-            
         }
     }
 
     //番を作成
-
     public void StrBoard(){
 
         for (int i = 0; i < 9; i++) {
